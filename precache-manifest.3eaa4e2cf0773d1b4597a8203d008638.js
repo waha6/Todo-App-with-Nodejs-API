e7f65d5fb51450d6cb9d4d6229b40ae4c924f908
Abdul Wahab Khatri <a6dulwahab@gmail.com>
2019-01-17 23:28:55 +0500

    Updates

---
+++ precache-manifest.3eaa4e2cf0773d1b4597a8203d008638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b96369922a7e47bd82a0",
+    "url": "/todoAppWithAPI/static/js/runtime~main.b9636992.js"
+  },
+  {
+    "revision": "a9b2e435cdddc8d94b17",
+    "url": "/todoAppWithAPI/static/js/main.a9b2e435.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/todoAppWithAPI/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "a9b2e435cdddc8d94b17",
+    "url": "/todoAppWithAPI/static/css/main.54eb8b19.chunk.css"
+  },
+  {
+    "revision": "24776f886054d4b8b3deb6568d8a333d",
+    "url": "/todoAppWithAPI/index.html"
+  }
+];
